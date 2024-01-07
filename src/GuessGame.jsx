69c00b3a10c7f3d608ebef9ecdc54196d246d0f6
dielawn <dielawnmerkill@gmail.com>
2024-01-07 15:19:19 -0700

    pick a hand is fully functional, useState for result, score, selectedHand, and total count, useEffect calls addPoint if result and selectedHand are equal

--- src/GuessGame.jsx
+++ src/GuessGame.jsx
@@ -1,51 +1,46 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Player } from "./Players"
 
-// class Game {
-//     constructor() {
-//         this.player1 = new Player('p1')
-//         this.player2 = new Player('p2')
-//         this.currentPlayer = null
-//     }
-//     switchPlayers(){
-        
-//         if (this.currentPlayer === null) {
-//             this.player1.currentPlayer = true;
-//             this.player2.currentPlayer = false;
-//             this.currentPlayer = this.player1
-//         } else {
-//             this.player1.currentPlayer = !this.player1.currentPlayer
-//             this.player2.currentPlayer = !this.player2.currentPlayer
-//             const isPlayer1 = this.player1.currentPlayer
-//             isPlayer1? this.currentPlayer = this.player1 : this.currentPlayer = this.player2
-//         }
-        
-//     }
-// }
-// const newGame = new Game()
-
-// newGame.switchPlayers()
-// console.log(newGame.currentPlayer)
-// newGame.switchPlayers()
-// console.log(newGame.currentPlayer)
-// newGame.switchPlayers()
-// console.log(newGame.currentPlayer)
 
 function PickAHand() {
    const [result, setResult] = useState(null)
+   const [scoreP1, setScore] = useState(0)
+   const [selectedHand, selectHand] = useState(null)
+   const [count, setCount] = useState(0)
 
-    const isThisHand = () => {
-       const rndmResult = Math.random() > .5 ? `Left Hand!` : `Right Hand`
+   const addPoint = () => {
+        if (result != null && selectedHand != null && result === selectedHand) {
+                setScore((prevScore) => prevScore + 1)    
+        }             
+   }
+   
+  
+    const isThisHand = (txt) => {
+       const rndmResult = Math.random() > .5 ? `Left Hand` : `Right Hand`
+       selectHand(txt)
        setResult(rndmResult)
+       setCount((count) => count + 1)
     }
- 
+    useEffect(() => {
+        addPoint()
+    }, [result, selectedHand])
 
     return (
         <>
+            
+
+
             <h2>Pick A Hand Any Hand</h2>
-            <button onClick={() => isThisHand()}>Left Hand</button>
-            <button onClick={() => isThisHand()}>Right Hand</button>
-            <p>{result}</p>
+            <button onClick={() => isThisHand('Left Hand')}>Left Hand</button>
+            <button onClick={() => isThisHand('Right Hand')}>Right Hand</button>
+            <p>You picked {selectedHand}</p>
+            <p>Answer: {result}</p>
+            <Player 
+                name="P1"
+                result={scoreP1}
+                addPoint={addPoint}
+             />
+             <p>Total Rounds played: {count}</p>
         </>
     )
 }
--- src/Players.jsx
+++ src/Players.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react"
 
-class Player {
-    constructor(name) {
-        this.name = name
-        this.currentPlayer = null
-        this.score = null
-    }
+// class Player {
+//     constructor(name) {
+//         this.name = name
+//         this.currentPlayer = null
+//         this.score = null
+//     }
+// }
+
+const Player = ({ result}) => {
+
+
+    return (
+        <>
+        <p>Score: {result}</p>        
+        </>
+     )
 }
+ 
 
 export {Player}
