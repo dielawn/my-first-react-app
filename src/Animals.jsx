8d91f2ec75ef327ea5c0a4a99c02bbd1db177cff
dielawn <dielawnmerkill@gmail.com>
2024-01-09 06:01:18 -0700

    used useState to check an input for a state change, if change results are rendered in a ul

--- src/Animals.jsx
+++ src/Animals.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 function ListItem(props) {
     return <li>{props.animal}</li>
 }
@@ -25,14 +26,39 @@ function ListByLetter(props) {
     )
 }
 
+function SearchByLetter(props) {
+    const [searchInput, setSearchInput] = useState('')
+    
+    const handleInputChange = (event) => {
+        setSearchInput(event.target.value)
+    }
+    
+
+    return (
+        <>
+            <input type="text" onChange={handleInputChange} placeholder="Search by letter" />
+           
+            <ul>
+                { props.animals
+                .filter((animal) => animal.startsWith(searchInput))
+                .map((animal) => (
+            <li key={animal}>{animal}</li>
+        ))}
+            </ul>
+        </>
+    )
+
+}
+
 export function Animals() {
-    const animals = ['Lion', 'Cow', 'Snake', 'Lizard']
+    const [animals, setAnimals]= useState(['Lion', 'Cow', 'Snake', 'Lizard'])
    
     return (
         <div>
             <h1>Animals:</h1>
             <List animals={animals} />
             <ListByLetter animals={animals}/>
+            <SearchByLetter animals={animals} />
         </div>
     )
 }
